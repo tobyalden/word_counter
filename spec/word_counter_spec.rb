f2042b73b6685b5b7771bb13d4a01ac775fccbf2
Toby Alden <toby.alden@gmail.com>
2015-04-24 11:06:24 -0700

    Adds spec for input phrases using capitalization. Spec fails as expected.

--- spec/word_counter_spec.rb
+++ spec/word_counter_spec.rb
@@ -8,4 +8,7 @@ describe('String#word_counter') do
   it("Returns the number of instances of the word word_counter was called on in the string word_counter was passed, accounting for punctuation.") do
     expect("hotel".word_counter("The hotel was a bad hotel, so we went to a different hotel.")).to(eq(3))
   end
+  it("Returns the number of instances of the word word_counter was called on in the string word_counter was passed, accounting for punctuation and capitalization.") do
+    expect("hotel".word_counter("The Grand Central Hotel was a bad hotel, so we went to the Riverside Hotel.")).to(eq(3))
+  end
 end
