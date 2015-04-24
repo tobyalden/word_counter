e3ded898ee45d6631f52b649f6776b115b53b345
Toby Alden <toby.alden@gmail.com>
2015-04-24 11:01:49 -0700

    Adds spec for input phrases using punctuation. Spec fails as expected.

--- spec/word_counter_spec.rb
+++ spec/word_counter_spec.rb
@@ -5,4 +5,7 @@ describe('String#word_counter') do
   it("Returns the number of instances of the word word_counter was called on in the string word_counter was passed.") do
     expect("hotel".word_counter("the hotel was a good hotel")).to(eq(2))
   end
+  it("Returns the number of instances of the word word_counter was called on in the string word_counter was passed, accounting for punctuation.") do
+    expect("hotel".word_counter("The hotel was a bad hotel, so we went to a different hotel.")).to(eq(3))
+  end
 end
